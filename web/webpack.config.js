bce9874ffe20f228bf9228f966fe0bf2d363b4d8
Antoshkin Artem <Artem.Antoshkin@infowatch.com>
2024-04-01 23:43:04 +0300

    * web: login form styles;

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -27,21 +27,11 @@ module.exports = {
   },
   plugins: [
     new ModuleFederationPlugin({
-      // library: { type: "module" },
-
-      // For remotes (please adjust)
       name: "au",
       filename: "remoteEntry.js",
       exposes: {
         './Module': './src/app/auth/auth.module.ts',
       },
-
-      // For hosts (please adjust)
-      // remotes: {
-      //     "mfe1": "http://localhost:3000/remoteEntry.js",
-
-      // },
-
       shared: share({
         "@angular/core": { singleton: true, strictVersion: true, requiredVersion: 'auto' },
         "@angular/common": { singleton: true, strictVersion: true, requiredVersion: 'auto' },
