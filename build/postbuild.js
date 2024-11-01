e3205d9521eb929c1a83512277e98245d74c9607
Artanty <thomasway7@gmail.com>
2024-11-02 00:50:42 +0300

    * web: AU-0001 +favicon;

--- build/postbuild.js
+++ build/postbuild.js
@@ -1,10 +1,5 @@
 const { zip } = require('./scripts/zip')
 
-// saveToDb({
-//   discId: '11zSVRVIz9Y43fsPbHfwBv2RVDMOJgJT4',
-//   project: process.env.APP
-// })
-
 /**
  * @params distFolderPath - путь до папки, в которой будет собран проект
  * пример:
